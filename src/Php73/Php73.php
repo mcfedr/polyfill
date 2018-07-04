872c12569534cb9762e054b88c61e1d0d36cfffe
Fred Cox <mcfedr@gmail.com>
2018-07-04 21:01:34 +0300

    separate array and float

--- src/Php73/Php73.php
+++ src/Php73/Php73.php
@@ -16,6 +16,8 @@ final class Php73
 
     private static $startAt = null;
 
+    private static $startAtArr = null;
+
     /**
      * @param bool $asNum
      *
@@ -23,31 +25,39 @@ final class Php73
      */
     public static function hrtime($asNum = false)
     {
-        if (null === self::$startAt) {
-            self::$startAt = microtime(true);
-            if (\PHP_INT_SIZE !== 4) {
-                // In this case $startAt is a int, number of micro seconds
-                self::$startAt = (int) (self::$startAt * self::MSEC_IN_SEC);
-            }
-        }
-
-        if (\PHP_INT_SIZE === 4) {
-            // Floor removes rounding errors from floating point
-            $nanos = floor((microtime(true) - self::$startAt) * self::NANO_IN_SEC);
-        } else {
-            $nowNanos = (int)(microtime(true) * self::MSEC_IN_SEC);
-
-            $nanos = ($nowNanos - self::$startAt) * self::NANO_IN_MSEC;
+        if (null === self::$startAtArr) {
+            self::$startAtArr = self::microtime();
         }
 
         if ($asNum) {
-            return $nanos;
+            if (null === self::$startAt) {
+                self::$startAt = self::$startAtArr[0] + self::$startAtArr[1];
+                if (\PHP_INT_SIZE !== 4) {
+                    // In this case $startAt is a int, number of micro seconds
+                    self::$startAt = (int) (self::$startAt * self::MSEC_IN_SEC);
+                }
+            }
+
+            if (\PHP_INT_SIZE === 4) {
+                // Floor removes rounding errors from floating point
+                return floor((microtime(true) - self::$startAt) * self::NANO_IN_SEC);
+            }
+            $nowNanos = (int) (microtime(true) * self::MSEC_IN_SEC);
+
+            return ($nowNanos - self::$startAt) * self::NANO_IN_MSEC;
         }
 
-        $secs = (int) ($nanos / self::NANO_IN_SEC);
-        $nanosPart = (int) $nanos - ($secs * self::NANO_IN_SEC);
+        $time = self::microtime();
 
-        return array($secs, $nanosPart);
+        $secs = $time[1] - self::$startAt[1];
+        // $msecs is in seconds, but its the fractional part i.e. 0.x
+        $msecs = $time[0] - self::$startAt[0];
+        if ($msecs < 0) {
+            $msecs += 1;
+            $secs -= 1;
+        }
+
+        return array($secs, (int) ($msecs * self::NANO_IN_SEC));
     }
 
     private static function microtime()
